7af196bbe5496e1a2f707b87c318bb9997dc917f
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-10-24 13:04:51 -0500

    pending check axios mocking it doesn't work correctly

--- second-snapshot/src/__mocks__/axios.js
+++ second-snapshot/src/__mocks__/axios.js
@@ -20,6 +20,13 @@ const axios = {
       }
     }
   }),
+  // get: jest.fn().mockImplementation(() => Promise.resolve({
+  //   data: {
+  //     photos: {
+  //       photo: []
+  //     }
+  //   }
+  // })),
   post: jest.fn().mockImplementation(() => Promise.reject("")),
   put: jest.fn()
 }
--- second-snapshot/src/__tests__/components/Container.test.js
+++ second-snapshot/src/__tests__/components/Container.test.js
@@ -2,8 +2,9 @@
 import { act, create } from 'react-test-renderer';
 import Container from '../../components/Container';
 import Gallery from '../../components/Gallery';
-import Loader from '../../components/Loader';
+// import Loader from '../../components/Loader';
 import PhotoContextProvider from '../../context/PhotoContext';
+import axios from "axios";
 
 let component;
 
@@ -25,11 +26,34 @@ describe('<Container />', () => {
   });
 
   it('Llama a la API si es necesario o cambia el texto', async () => {
+    const customData = {
+      photos: {
+        photo: [
+          {
+            farm: "farmTest01",
+            server: "serverTest",
+            id: "testId01", secret: "1212312321eaw",
+            title: "titleTest01"
+          }
+        ]
+      }
+    };
+    // axios.get.mockImplementation(() => Promise.resolve({customData }));
+    axios.get(() => Promise.resolve({
+      customData
+    }));
+
     await act(async () => {
       await component.update(
         <PhotoContextProvider>
           <Container searchTerm="text" />
         </PhotoContextProvider>);
     });
+
+    // expect(axios.get).toHaveBeenCalled();
+    // expect(axios.get).toHaveBeenCalledTimes(3);
+    expect(axios.post).not.toHaveBeenCalled();
+    expect(axios.put).not.toHaveBeenCalled();
+    // expect(component.root.findByType(Gallery).props.data).toEqual(customData.photos.photo);
   });
 });
